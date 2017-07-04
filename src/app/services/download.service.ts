5cd2642a5c47f49db8e7dac6ad8031bfb019c3fe
gerlis18 <gerlis1997@gmail.com>
2017-07-04 18:21:08 -0500

    borrado file-saver de las dependencias

--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import * as FileSaver from 'file-saver';
 import 'rxjs/Rx';
 
 @Injectable()
